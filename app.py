0a2dc5463f3078c3e8bc4627c9bc598f0fde18cb
ArtisticWenny <141374294+ArtisticWenny@users.noreply.github.com>
2023-11-05 17:39:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 from flask import Flask, render_template
-from db import Base, Patient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 app = Flask(__name__)
 
-# # Create a SQLite database (you can change this to your specific database)
-# # DATABASE_URL = "mysql+pymysql://hants:ahi-admin-2023@scratch-server.mysql.database.azure.com/hants"
-# DATABASE_URL = "mysql+mysqlconnector://hants:INSERT-HERE@scratch-server.mysql.database.azure.com/hants"
-DATABASE_URL = "sqlite:///local.db"
+# Create a SQLite database (you can change this to your specific database)
+# DATABASE_URL = "mysql+mysqlconnector://wenny:Cakelover1@34.172.190.54:3306/GCP4b"
+DATABASE_URL = 'mysql+pymysql://wenny:Cakelover1@34.172.190.54:3306/GCP4b'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-Base.metadata.bind = app
+#app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL#
+#Base.metadata.bind = app
 
 # Create a SQLAlchemy engine and session
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 @app.route('/')
 def index():
     # Retrieve data from the database
-    patients = session.query(Patient).all()
-    return render_template('index_tailwind.html', patients=patients)
+    patients = session.query(patients).all()
+    return render_template('index.html', patients=patients)
 
 if __name__ == '__main__':
     app.run(debug=True)
